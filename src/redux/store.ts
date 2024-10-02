89c9850bac98aab82e7f9617755687db60656824
sumonpaul55 <sumpaul100@gmail.com>
2024-10-02 21:43:22 +0600

    modified store to connect auth slice with persistor

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,15 +1,31 @@
-import storage from "redux-persist/lib/storage";
 import { configureStore } from "@reduxjs/toolkit";
+import authReducer from "./features/auth/authSlice";
+import storage from "redux-persist/lib/storage";
+import { persistStore, persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from "redux-persist";
+import { baseApi } from "./api/baseApi";
 const persistConfig = {
   key: "auth",
   storage,
 };
 
+const persistAuthReducer = persistReducer(persistConfig, authReducer);
+
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    [baseApi.reducerPath]: baseApi.reducer,
+    auth: persistAuthReducer,
+  },
+  middleware: (getDefaultMiddlewares) =>
+    getDefaultMiddlewares({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }).concat(baseApi.middleware),
 });
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>;
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 export type AppDispatch = typeof store.dispatch;
+
+export const persistor = persistStore(store);
