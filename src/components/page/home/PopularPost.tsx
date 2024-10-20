a4b62c9aa54f6af3e3fdf624ad655d09e92fa6ba
sumonpaul55 <sumpaul100@gmail.com>
2024-10-20 21:05:11 +0600

    hyepar link from popular post

--- src/components/page/home/PopularPost.tsx
+++ src/components/page/home/PopularPost.tsx
@@ -5,7 +5,7 @@ import React from 'react'; // Import the Post component
 import EachPopularPost from './EachPopularPost';
 import { Tpost } from '@/types';
 import LoadingSpinner from '@/components/shared/LoadingSpinner';
-import Container from '@/components/shared/Container/Container';
+import Link from 'next/link';
 
 const PopularPosts = () => {
     // Sort posts by upvotes and get the top 3
@@ -17,14 +17,16 @@ const PopularPosts = () => {
             <div>
                 {
                     isLoading ? <LoadingSpinner /> :
-                        <div className="my-8 p-6 rounded-lg bg-white py-20">
-                            <h2 className="text-3xl font-bold text-center mb-6">Popular Posts</h2>
-                            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-9">
-                                {upVotesPost?.map((post: Tpost) => (
-                                    <EachPopularPost key={post._id} post={post} />
-                                ))}
+                        <Link href={`/post/${posts._id}`}>
+                            <div className="my-8 p-6 rounded-lg bg-white py-20">
+                                <h2 className="text-3xl font-bold text-center mb-6">Popular Posts</h2>
+                                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-9">
+                                    {upVotesPost?.map((post: Tpost) => (
+                                        <EachPopularPost key={post._id} post={post} />
+                                    ))}
+                                </div>
                             </div>
-                        </div>
+                        </Link>
                 }
             </div>
         </>
